2b4572af1e07a8131f33be27dc22634f49e57b89
Pedro <pedrohsareis@gmail.com>
2018-11-26 19:57:28 -0200

    Função de batalha retornando string

--- src/pokemon/Batalha.java
+++ src/pokemon/Batalha.java
@@ -20,40 +20,68 @@ public class Batalha {
         this.pokemon1 = pokemon1;
         this.pokemon2 = pokemon2;
     }
-    public void batalhar(){
+    public String batalhar(){
+        
+        StringBuilder saida = new StringBuilder();
+        
         while(pokemon1.getHp() >= 0 && pokemon2.getHp() >= 0){
             if(pokemon1.getHp() > 0){
-                System.out.println(pokemon1.getNome() + " irá atacar " + pokemon2.getNome());
+                saida.append(pokemon1.getNome());
+                saida.append(" irá atacar ");
+                saida.append(pokemon2.getNome());
+                saida.append(System.lineSeparator());
                 ataque = rand.nextInt(pokemon1.getAtaque()) + 1;
                 if(ataque > (pokemon1.getAtaque() / 2)){
-                    System.out.println("O ataque foi efetivo.");
-                    System.out.println(pokemon2.getNome() + " perdeu 5 pontos de vida.");
+                    saida.append("O ataque foi efetivo.");
+                    saida.append(System.lineSeparator());
+                    saida.append(pokemon2.getNome());
+                    saida.append(" perdeu 5 pontos de vida.");
+                    saida.append(System.lineSeparator());
                     hp = pokemon2.getHp();
                     pokemon2.setHp(hp - 5);
                 }else{
-                    System.out.println("O ataque não foi efetivo.");
-                    System.out.println(pokemon2.getNome() + " não perdeu pontos de vida.");
+                    saida.append("O ataque não foi efetivo.");
+                    saida.append(System.lineSeparator());
+                    saida.append(pokemon2.getNome());
+                    saida.append(" não perdeu pontos de vida.");
+                    saida.append(System.lineSeparator());
                 }
             }else{
-                System.out.println(pokemon1.getNome() + " foi derrotado.");
+                saida.append(System.lineSeparator());
+                saida.append(pokemon1.getNome());
+                saida.append(" foi derrotado.");
+                saida.append(System.lineSeparator());
                 break;
             }
             if(pokemon2.getHp() > 0){
-                System.out.println(pokemon2.getNome() + " irá atacar " + pokemon1.getNome());
+                saida.append(pokemon2.getNome());
+                saida.append(" irá atacar ");
+                saida.append(pokemon1.getNome());
+                saida.append(System.lineSeparator());
                 ataque = rand.nextInt(pokemon2.getAtaque()) + 1;
                 if(ataque > (pokemon2.getAtaque() / 2)){
-                    System.out.println("O ataque foi efetivo.");
-                    System.out.println(pokemon1.getNome() + " perdeu 5 pontos de vida.");
+                    saida.append("O ataque foi efetivo.");
+                    saida.append(System.lineSeparator());
+                    saida.append(pokemon1.getNome());
+                    System.out.println(" perdeu 5 pontos de vida.");
+                    saida.append(System.lineSeparator());
                     hp = pokemon1.getHp();
                     pokemon1.setHp(hp - 5);
                 }else{
-                    System.out.println("O ataque não foi efetivo.");
-                    System.out.println(pokemon1.getNome() + " não perdeu pontos de vida.");
+                    saida.append("O ataque não foi efetivo.");
+                    saida.append(System.lineSeparator());
+                    saida.append(pokemon1.getNome());
+                    saida.append(" não perdeu pontos de vida.");
+                    saida.append(System.lineSeparator());
                 }
             }else{
-                System.out.println(pokemon2.getNome() + " foi derrotado.");
+                saida.append(System.lineSeparator());
+                saida.append(pokemon2.getNome());
+                saida.append(" foi derrotado.");
+                saida.append(System.lineSeparator());
                 break;
             }
         }
+        return saida.toString();
     }
 }
