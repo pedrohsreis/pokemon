c4a98ae064c98d9d7cb8c92600234e2f26a974f9
rodrigokameronn <42418886+rodrigokameronn@users.noreply.github.com>
2018-11-30 17:18:40 -0200

    Add files via upload

---
+++ src/Jogador/Jogador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Jogador;
+
+/**
+ *
+ * @author ice
+ */
+public class Jogador {
+    
+    private String nome;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }   
+    
+}
